8c3b9049f0bab604ad678a68a22b62cc4f7ac0b5
sn0w12 <lucasskold49@gmail.com>
2024-04-15 15:46:17 +0200

    Added function to get all available cars

--- src/main/java/com/example/enterprisefall/service/CarService.java
+++ src/main/java/com/example/enterprisefall/service/CarService.java
@@ -1,10 +1,14 @@
 package com.example.enterprisefall.service;
 
+import com.example.enterprisefall.entity.Car;
 import com.example.enterprisefall.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import org.apache.log4j.Logger;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CarService {
@@ -17,5 +21,10 @@ public class CarService {
     this.carRepository = carRepository;
   }
 
-  // METODER
+  // Method to fetch all available (non-booked) cars
+  public List<Car> findAllAvailableCars() {
+    return carRepository.findAll().stream()
+            .filter(car -> !car.getIsBooked())
+            .collect(Collectors.toList());
+  }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO cars (brand, model, registration_number, price_per_day, is_booked) VALUES ('Toyota', 'Corolla', 'REG123', 55.0, false);
+INSERT INTO cars (brand, model, registration_number, price_per_day, is_booked) VALUES ('Ford', 'Fiesta', 'REG124', 45.0, false);
+INSERT INTO cars (brand, model, registration_number, price_per_day, is_booked) VALUES ('Tesla', 'Model 3', 'REG125', 85.0, true);
+INSERT INTO cars (brand, model, registration_number, price_per_day, is_booked) VALUES ('Hyundai', 'i10', 'REG126', 40.0, false);
+INSERT INTO cars (brand, model, registration_number, price_per_day, is_booked) VALUES ('BMW', '320i', 'REG127', 70.0, true);
