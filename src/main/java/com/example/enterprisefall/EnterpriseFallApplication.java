10cb694220f56cdd684d70e5fc1aab3734694649
sn0w12 <lucasskold49@gmail.com>
2024-04-15 13:07:06 +0200

    Init

---
+++ src/main/java/com/example/enterprisefall/EnterpriseFallApplication.java
@@ -0,0 +1,13 @@
+package com.example.enterprisefall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseFallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseFallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enterprisefall/EnterpriseFallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enterprisefall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseFallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
