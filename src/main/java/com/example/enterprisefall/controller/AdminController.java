8b2d7ba7d1a552e6ac4aba1693bbd3ba5481c737
sn0w12 <lucasskold49@gmail.com>
2024-04-16 13:50:56 +0200

    Add imports

--- src/main/java/com/example/enterprisefall/controller/AdminController.java
+++ src/main/java/com/example/enterprisefall/controller/AdminController.java
@@ -8,9 +8,12 @@ import com.example.enterprisefall.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 //Administratörer ska kunna göra ett antal aktiviteter med följande endpoints:
 //        • Lista kunder GET /api/v1/customers
