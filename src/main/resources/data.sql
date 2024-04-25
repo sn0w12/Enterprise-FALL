e823d805f2250c98d4e9f0b61dabab3a1d277535
koifishlionfish <laxelsson96@gmail.com>
2024-04-25 11:04:47 +0200

    Fixat så att det finns tre aktiva bokningar för customerId 1-3

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
---Cars testdata
+-- Cars testdata
 INSERT INTO cars (brand, model, registration_number, price_per_day, is_booked)
 VALUES ('Toyota', 'Corolla', 'REG123', 55.0, false),
        ('Ford', 'Fiesta', 'REG124', 45.0, false),
@@ -6,16 +6,16 @@ VALUES ('Toyota', 'Corolla', 'REG123', 55.0, false),
        ('Hyundai', 'i10', 'REG126', 40.0, false),
        ('BMW', '320i', 'REG127', 70.0, true);
 
---Booking testdata
-INSERT INTO Booking (booking_date, return_date)
-VALUES ('2024-04-16', '2024-04-20'),
-       ('2024-04-18', '2024-04-25'),
-       ('2024-04-20', '2024-04-24');
-
---Customer testdata
+-- Customer testdata
 INSERT INTO customers (username, name, address, email, phone_number)
 VALUES ('jdoe', 'John Doe', '1234 Elm Street', 'johndoe@example.com', '555-1234'),
        ('asmith', 'Alice Smith', '2345 Oak Street', 'alicesmith@example.com', '555-2345'),
        ('rjones', 'Robert Jones', '3456 Pine Street', 'robertjones@example.com', '555-3456'),
        ('emiller', 'Emma Miller', '4567 Maple Street', 'emmamiller@example.com', '555-4567'),
        ('twilliams', 'Tom Williams', '5678 Spruce Street', 'tomwilliams@example.com', '555-5678');
+
+-- Booking testdata
+INSERT INTO Booking (booking_date, return_date, customer_id, car_id)
+VALUES (CURRENT_DATE, CURRENT_DATE + INTERVAL '7' DAY, 1, 1),
+       (CURRENT_DATE, CURRENT_DATE + INTERVAL '7' DAY, 2, 2),
+       (CURRENT_DATE, CURRENT_DATE + INTERVAL '7' DAY, 3, 3);
